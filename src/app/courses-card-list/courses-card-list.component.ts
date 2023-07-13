ff6612cc7010454faab8bea47c6b678a360abd0e
dikiogres <dikiogres@gmail.com>
2023-07-13 14:58:00 +0700

    feat: responsive vreakpoint observer service

--- src/app/courses-card-list/courses-card-list.component.ts
+++ src/app/courses-card-list/courses-card-list.component.ts
@@ -1,8 +1,9 @@
 import {Component, Input, OnInit, ViewEncapsulation} from '@angular/core';
-import {Course} from "../model/course";
-import { MatDialog, MatDialogConfig } from "@angular/material/dialog";
-import { openEditCourseDialog } from '../course-dialog/course-dialog.component';
-import { filter } from 'rxjs/operators';
+import {Course} from '../model/course';
+import {MatDialog, MatDialogConfig} from '@angular/material/dialog';
+import {openEditCourseDialog} from '../course-dialog/course-dialog.component';
+import {filter} from 'rxjs/operators';
+import {BreakpointObserver, Breakpoints} from '@angular/cdk/layout';
 
 @Component({
     selector: 'courses-card-list',
@@ -20,26 +21,61 @@ export class CoursesCardListComponent implements OnInit {
 
     handsetPortrait = false;
 
-    constructor(private dialog: MatDialog) {
+
+    constructor(private dialog: MatDialog,
+                private responsive: BreakpointObserver) {
     }
 
     ngOnInit() {
 
+        this.responsive.observe([
+            Breakpoints.TabletPortrait,
+            Breakpoints.TabletLandscape,
+            Breakpoints.HandsetPortrait,
+            Breakpoints.HandsetLandscape
+        ])
+            .subscribe(result => {
+
+                this.cols = 3;
+                this.rowHeight = "500px";
+                this.handsetPortrait = false;
+
+                const breakpoints = result.breakpoints;
+
+                if (breakpoints[Breakpoints.TabletPortrait]) {
+                    this.cols = 1;
+                }
+                else if (breakpoints[Breakpoints.HandsetPortrait]) {
+                    this.cols = 1;
+                    this.rowHeight = "430px";
+                    this.handsetPortrait = true;
+                }
+                else if (breakpoints[Breakpoints.HandsetLandscape]) {
+                    this.cols = 1;
+                }
+                else if (breakpoints[Breakpoints.TabletLandscape]) {
+                    this.cols = 2;
+                }
+
+            });
+
+
     }
 
-    editCourse(course:Course) {
+    editCourse(course: Course) {
+
         openEditCourseDialog(this.dialog, course)
-        .pipe(
-            filter(val => !!val),
-        )
-        .subscribe(
-            val => console.log("new course value:",val)
-        );
+            .pipe(
+                filter(val => !!val)
+            )
+            .subscribe(
+                val => console.log('new course value:', val)
+            );
+
 
     }
 
 }
-
 
 
 
