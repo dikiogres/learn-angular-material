26878956b3564b8994d31ad8c1426d2bffe8e45c
dikiogres <dikiogres@gmail.com>
2023-07-13 14:52:13 +0700

    feat: drag and drop  between different list

--- src/app/drag-drop/drag-drop.component.ts
+++ src/app/drag-drop/drag-drop.component.ts
@@ -90,6 +90,24 @@ export class DragDropComponent {
     }
   ];
 
+  done = [];
+
+  dropMultiList(event: CdkDragDrop<Lesson[]>) {
+
+    if (event.previousContainer == event.container) {
+        moveItemInArray(this.lessons, event.previousIndex, event.currentIndex);
+    }
+    else {
+        transferArrayItem(
+            event.previousContainer.data,
+            event.container.data,
+            event.previousIndex,
+            event.currentIndex
+
+        );
+    }
+
+}
   drop(event: CdkDragDrop<Lesson[]>){
     
     moveItemInArray(this.lessons, event.previousIndex, event.currentIndex);
