a917f59d219ee5fc79eb8b9f586a62db36e66cee
dikiogres <dikiogres@gmail.com>
2023-07-13 13:10:41 +0700

    feat: reverse page indeex on refresh

--- src/app/course/course.component.ts
+++ src/app/course/course.component.ts
@@ -65,6 +65,8 @@ export class CourseComponent implements OnInit, AfterViewInit {
 
     ngAfterViewInit() {
 
+      this.sort.sortChange.subscribe(()=>this.paginator.pageIndex=0);
+
       merge(this.sort.sortChange, this.paginator.page)
       .pipe(
         tap(()=>this.loadLessonsPage())
